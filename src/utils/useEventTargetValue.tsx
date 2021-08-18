2796f5f4e15ab2b49aca2da662ca46501101ff92
KIMBEOBWOO <qjqdn1568@naver.com>
2021-08-18 10:06:50 +0900

    seconde commit

---
+++ src/utils/useEventTargetValue.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+
+export default function useEventTargetValue(defaultValue = ''): {
+  value: string;
+  handleChange(e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void;
+  setValue: (value: React.SetStateAction<string>) => void;
+  handleChangePhoneNumber?(e: React.ChangeEvent<HTMLInputElement>): void;
+  handleChangeISODateNumber?(e: React.ChangeEvent<HTMLInputElement>): void;
+} {
+  const [value, setValue] = React.useState(defaultValue);
+
+  /**
+   * @name 전화번호_포매터
+   * @param phoneNumber 전화번호, e.target.value <string>
+   * @returns xx-xxx-xxxx 혹은 xxx-xxxx-xxxx 꼴의 <string>
+   */
+  function inputPhoneNumber(phoneNumber: string): string {
+    const number = phoneNumber.replace(/[^0-9]/g, '');
+    let phone = '';
+    if (number.length < 4) {
+      return number;
+    } if (number.length < 7) {
+      phone += number.substr(0, 3);
+      phone += '-';
+      phone += number.substr(3);
+    } else if (number.length < 11) {
+      phone += number.substr(0, 3);
+      phone += '-';
+      phone += number.substr(3, 3);
+      phone += '-';
+      phone += number.substr(6);
+    } else {
+      phone += number.substr(0, 3);
+      phone += '-';
+      phone += number.substr(3, 4);
+      phone += '-';
+      phone += number.substr(7);
+    }
+    return phone;
+  }
+
+  /**
+   * @name 날짜_포매터
+   * @param dateString UTC Date String, e.target.value <string>
+   * @returns YYYY-MM-DD 꼴의 <string>
+   */
+  function inputISODate(dateString: string): string {
+    const number = dateString.replace(/[^0-9]/g, '');
+    let date = '';
+    if (number.length < 5) {
+      return number;
+    } if (number.length < 7) {
+      date += number.substr(0, 4);
+      date += '-';
+      date += number.substr(4, 5);
+    } else if (number.length < 9) {
+      date += number.substr(0, 4);
+      date += '-';
+      date += number.substr(4, 2);
+      date += '-';
+      date += number.substr(6, 2);
+    } else {
+      date += number.substr(0, 4);
+      date += '-';
+      date += number.substr(4, 2);
+      date += '-';
+      date += number.substr(6);
+    }
+    return date;
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void {
+    e.preventDefault();
+    setValue(e.target.value);
+  }
+
+  function handleChangePhoneNumber(e: React.ChangeEvent<HTMLInputElement>): void {
+    e.preventDefault();
+    setValue(inputPhoneNumber(e.target.value));
+  }
+
+  function handleChangeISODateNumber(e: React.ChangeEvent<HTMLInputElement>): void {
+    e.preventDefault();
+    setValue(inputISODate(e.target.value));
+  }
+
+
+  return {
+    value,
+    handleChange,
+    setValue,
+    handleChangePhoneNumber,
+    handleChangeISODateNumber,
+  };
+}
